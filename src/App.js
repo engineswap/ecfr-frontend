510da6846e2dd65bc7efc65525e14ae2a8720a3a
Ilya <yzybot1@gmail.com>
2025-02-13 15:05:45 -0600

    added icon and tab title

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import React, { useEffect } from "react";
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Analysis from './pages/Analysis';
@@ -8,6 +9,13 @@ import Search from './pages/Search';
 import SearchResultPage from './pages/SearchResultPage';
 
 function App() {
+    useEffect(() => {
+        document.title = "eCFR Analyzer";
+        const favicon = document.querySelector("link[rel='icon']");
+        if (favicon) {
+            favicon.href = "https://cdn-icons-png.flaticon.com/512/4252/4252349.png";
+        }
+    }, []);
     return (
         <Router>
             <div className="bg-gray-900 text-white h-screen flex flex-col">
@@ -16,10 +24,10 @@ function App() {
                     <Routes>
                         <Route path="/" element={<Home />} />
                         <Route path="/title/:titleId/:issueDate" element={<TitleStructure />} />
-                        <Route path="/amendments/:titleId" element={<Amendments/>} />
+                        <Route path="/amendments/:titleId" element={<Amendments />} />
                         <Route path="/analysis" element={<Analysis />} />
-                        <Route path="/search_result" element={<SearchResultPage/>} />
-                        <Route path="/search" element={<Search/>} />
+                        <Route path="/search_result" element={<SearchResultPage />} />
+                        <Route path="/search" element={<Search />} />
                     </Routes>
                 </div>
             </div>
